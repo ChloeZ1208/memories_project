45b739d93cd5b7d7ea24504e301142d9460b42fb
ChloeZ1208 <dongqingzhu1208@hotmail.com>
2021-11-17 12:27:18 -0500

    init:backend&frontend&MongoDB setup and database connection

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser  from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true })); // set pic upload limit
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://chloeMemories:chloeMemories@cluster0.my7yu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+// return a promise
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))) // if connection successful
+  .catch((error) => console.log(error.message)); // if connection fails
+
+
+// host database on mongoDB cloud
+// https://www.mongodb.com/cloud/atlas;
+
