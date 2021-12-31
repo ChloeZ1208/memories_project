57fa3aa81add3d5f0790dd462638de0233609391
ChloeZ1208 <dongqingzhu1208@hotmail.com>
2021-12-30 20:26:05 -0500

    finish p1&p2:create/delete memories, likes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js'
 
 const app = express();
-dotenv.config();
+// dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true })); // set pic upload limit
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,12 +15,12 @@ app.use(cors()); // enable the server to respond to preflight requests
 app.use('/posts', postRoutes); //posts can only be accessed w/ localhost:5000/posts
 
 const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = 'mongodb+srv://chloeMemories:chloeMemories@cluster0.my7yu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 // return a promise
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))) // if connection successful
   .catch((error) => console.log(error.message)); // if connection fails
-
 
 // host database on mongoDB cloud
 // https://www.mongodb.com/cloud/atlas;
